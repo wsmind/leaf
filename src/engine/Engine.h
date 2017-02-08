fdd8264518569a2b768571809d33321c6e9cc4fa
Remi Papillie <remi.papillie@gmail.com>
2017-02-08 22:03:07 +0100

    split engine template code into a separate header

--- src/engine/Engine.h
+++ src/engine/Engine.h
@@ -43,19 +43,4 @@ class Engine
         static Engine *getInstance() { assert(Engine::instance); return Engine::instance; }
 };
 
-template <typename ResourceType>
-void Engine::loadDataCollection(cJSON *json, const std::string &collectionName)
-{
-    cJSON *elements = cJSON_GetObjectItem(json, collectionName.c_str());
-    if (elements)
-    {
-        cJSON *element = elements->child;
-        while (element)
-        {
-            std::string resourceName = element->string;
-            ResourceManager::getInstance()->updateResourceData<ResourceType>(resourceName, element);
-
-            element = element->next;
-        }
-    }
-}
+#include <engine/Engine.inline.h>
