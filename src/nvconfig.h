e4ed44dc91f7f14e23a170ce5531ff1689b6ddbf
Remi Papillie <remi.papillie@gmail.com>
2017-02-17 14:17:51 +0100

    compiled nvcore in the texture compressor

---
+++ src/nvconfig.h
@@ -0,0 +1,22 @@
+#ifndef NV_CONFIG
+#define NV_CONFIG
+
+#define HAVE_UNISTD_H
+#define HAVE_STDARG_H
+#define HAVE_SIGNAL_H
+#define HAVE_EXECINFO_H
+#define HAVE_MALLOC_H
+
+#define HAVE_OPENMP
+#define HAVE_DISPATCH_H
+
+#define HAVE_STBIMAGE
+//#define HAVE_PNG
+//#define HAVE_JPEG
+//#define HAVE_TIFF
+//#define HAVE_OPENEXR
+//#define HAVE_FREEIMAGE
+
+#define HAVE_MAYA
+
+#endif // NV_CONFIG
