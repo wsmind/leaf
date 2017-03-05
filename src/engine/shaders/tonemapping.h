ae37236d96308e2dd6cbaaa14e99cb9054e94b4d
Remi Papillie <remi.papillie@gmail.com>
2017-03-05 12:53:54 +0100

    filmic tone mapping approximation

--- src/engine/shaders/tonemapping.h
+++ src/engine/shaders/tonemapping.h
@@ -11,3 +11,9 @@ float3 reinhardToneMapping(float3 color)
     color = color / (1.0 + color);
     return linearToGamma(color);
 }
+
+float3 filmicToneMapping(float3 color)
+{
+    float3 x = max(0, color - 0.004);
+    return (x * (6.2 * x + 0.5)) / (x * (6.2 * x + 1.7) + 0.06);
+}
