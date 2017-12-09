d2f42e91777908b87a79cdc4c789e73a586c9c91
Remi Papillie <remi.papillie@gmail.com>
2017-12-09 16:55:54 +0100

    fixed shadow mapping

--- src/engine/render/shaders/depthonly.vs.hlsl
+++ src/engine/render/shaders/depthonly.vs.hlsl
@@ -6,18 +6,14 @@ struct VS_INPUT
     float3 normal: NORMAL;
     float4 tangent: TANGENT;
     float2 uv: TEXCOORD;
-};
-
-cbuffer DepthOnlyConstants : register(b0)
-{
-    float4x4 transformMatrix;
+	float4x4 transformMatrix: TRANSFORM;
 };
 
 DEPTHONLY_PS_INPUT main(VS_INPUT input)
 {
     DEPTHONLY_PS_INPUT output;
 
-    output.position = mul(transformMatrix, float4(input.pos, 1.0));
+    output.position = mul(input.transformMatrix, float4(input.pos, 1.0));
 
     // hack; GL to DX clip space
     output.position.z = (output.position.z + output.position.w) * 0.5;
