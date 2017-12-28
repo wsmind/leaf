7b931e05793f0239c49f90b6ec99bce029f24803
Remi Papillie <remi.papillie@gmail.com>
2017-12-28 18:03:41 +0100

    bumped profiling limits

--- src/engine/render/graph/GPUProfiler.h
+++ src/engine/render/graph/GPUProfiler.h
@@ -46,7 +46,7 @@ class GPUProfiler
 
         // queries are preallocated and used dynamically during frames
         // (only D3D11_QUERY_TIMESTAMP queries are pooled)
-        static const unsigned int QUERY_POOL_SIZE = 2000;
+        static const unsigned int QUERY_POOL_SIZE = 10000;
         std::vector<ID3D11Query *> queryPool;
 
         struct ProfilePoint
