94eae212d0e6a8474ce058b43db2c9e574059b5d
Remi Papillie <remi.papillie@gmail.com>
2017-12-29 20:28:27 +0100

    fixed bloom threshold

--- src/engine/render/shaders/bloomthreshold.ps.hlsl
+++ src/engine/render/shaders/bloomthreshold.ps.hlsl
@@ -14,10 +14,11 @@ PixelOutput main(POSTPROCESS_PS_INPUT input)
 {
 	PixelOutput output;
 
-	float3 color = inputTexture.Sample(inputSampler, input.uv).rgb;
+	float3 color = inputTexture.Sample(inputSampler, input.uv + passConstants.viewportSize.zw * 0.25).rgb;
+	float luminance = computeLuminance(color);
 
 	float threshold = 1.0;
-	color = max(color - threshold, 0.0);
+	color *= smoothstep(0.0, threshold, luminance);
 
 	output.color = float4(color, 1.0);
 
