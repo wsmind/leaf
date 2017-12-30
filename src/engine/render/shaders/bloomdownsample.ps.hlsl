647ebac9b11544ce8d9944178f9bec439cb1d762
Remi Papillie <remi.papillie@gmail.com>
2017-12-30 20:24:50 +0100

    high quality downsampling for bloom

--- src/engine/render/shaders/bloomdownsample.ps.hlsl
+++ src/engine/render/shaders/bloomdownsample.ps.hlsl
@@ -2,6 +2,8 @@
 #include "postprocess.h"
 #include "scene.h"
 
+#define SAMPLE_COUNT 13
+
 Texture2D<float4> inputTexture: register(t0);
 SamplerState inputSampler: register(s0);
 
@@ -10,7 +12,7 @@ struct PixelOutput
 	float4 color: SV_TARGET;
 };
 
-float3 fetch4BilinearSamples(float2 uv)
+/*float3 fetch4BilinearSamples(float2 uv)
 {
 	float2 offset = 0.5 * passConstants.viewportSize.zw;
 
@@ -21,13 +23,52 @@ float3 fetch4BilinearSamples(float2 uv)
 	color += inputTexture.Sample(inputSampler, uv + float2(offset.x, -offset.y)).rgb;
 
 	return color * 0.25;
-}
+}*/
 
 PixelOutput main(POSTPROCESS_PS_INPUT input)
 {
 	PixelOutput output;
 
-	float3 color = fetch4BilinearSamples(input.uv);
+	float3 color = 0.0;
+
+	float2 offsets[SAMPLE_COUNT] = {
+		float2(-0.5, -0.5),
+		float2(-0.5, 0.5),
+		float2(0.5, 0.5),
+		float2(0.5, -0.5),
+		float2(-1.0, -1.0),
+		float2(-1.0, 1.0),
+		float2(1.0, 1.0),
+		float2(1.0, -1.0),
+		float2(-1.0, 0.0),
+		float2(1.0, 0.0),
+		float2(0.0, -1.0),
+		float2(0.0, 1.0),
+		float2(0.0, 0.0)
+	};
+
+	float weights[SAMPLE_COUNT] = {
+		0.5 / 4.0,
+		0.5 / 4.0,
+		0.5 / 4.0,
+		0.5 / 4.0,
+		0.125 / 4.0,
+		0.125 / 4.0,
+		0.125 / 4.0,
+		0.125 / 4.0,
+		0.125 / 2.0,
+		0.125 / 2.0,
+		0.125 / 2.0,
+		0.125 / 2.0,
+		0.125
+	};
+
+	[unroll]
+	for (int i = 0; i < SAMPLE_COUNT; i++)
+	{
+		color += weights[i] * inputTexture.Sample(inputSampler, input.uv + offsets[i] * passConstants.viewportSize.zw).rgb;
+	}
+
 	output.color = float4(color, 1.0);
 
 	return output;
