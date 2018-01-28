e6bf8f76c9aaa8f7c4fb27518752051cfa5373d4
Remi Papillie <remi.papillie@gmail.com>
2018-01-28 23:02:27 +0100

    displayed HDR envmap as background image

--- src/engine/render/shaders/background.ps.hlsl
+++ src/engine/render/shaders/background.ps.hlsl
@@ -13,11 +13,12 @@ PS_OUTPUT main(BACKGROUND_PS_INPUT input)
 	PS_OUTPUT output;
 
     float3 direction = normalize(input.worldPosition);
-    float3 sky = (direction.z * 0.5 + 0.5) * float3(0.8, 0.9, 1.0);
-    float sun = pow(saturate(dot(direction, normalize(float3(1.0, 1.0, 1.0))) + 0.005), 100.0);
-	output.color = float4(lerp(sky, float3(1.0, 0.9, 0.8), sun), 1.0);
 
-    output.color = environmentTexture.Sample(environmentSampler, direction.xy);
+    // convert ray to equirectangular coordinates
+    float x = (atan2(direction.y, -direction.x) / 3.141592) * 0.5 + 0.5;
+    float y = (asin(-direction.z) / 1.570796) * 0.5 + 0.5;
+
+    output.color = environmentTexture.Sample(environmentSampler, float2(x, y));
 
 	return output;
 }
