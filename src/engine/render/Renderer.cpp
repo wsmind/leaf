bd3b1fc94327da2fb9877680c8698da8fdbbf997
Remi Papillie <remi.papillie@gmail.com>
2018-02-07 06:49:54 +0100

    fixed random crash on engine initialization

--- src/engine/render/Renderer.cpp
+++ src/engine/render/Renderer.cpp
@@ -232,6 +232,10 @@ Renderer::Renderer(HWND hwnd, int backbufferWidth, int backbufferHeight, bool ca
         0x02, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00,
         0x03, 0x00, 0x00, 0x00,
+
+        // material name
+        0x09, 0x00, 0x00, 0x00,
+        "__default"
     };
 
     ResourceManager::getInstance()->updateResourceData<Mesh>("__fullscreenQuad", fullscreenQuadData, sizeof(fullscreenQuadData));
