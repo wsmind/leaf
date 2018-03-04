0d96abba786d7b29e88ab7f5713341936642b3f8
Remi Papillie <remi.papillie@gmail.com>
2018-03-04 19:43:25 +0100

    added particle spawn velocity

--- src/engine/scene/ParticleSystem.cpp
+++ src/engine/scene/ParticleSystem.cpp
@@ -77,7 +77,10 @@ void ParticleSystem::createSimulation()
 
         particle.visible = false;
         particle.spawnPosition = glm::ballRand(1.0f);
-        particle.spawnVelocity = glm::ballRand(0.1f);
+        particle.spawnVelocity = glm::ballRand(0.04f);
+
+        // cubify initial position
+        particle.spawnPosition /= max(max(abs(particle.spawnPosition.x), abs(particle.spawnPosition.y)), abs(particle.spawnPosition.z));
 
         particle.size = this->settings->size * (1.0f - glm::linearRand(0.0f, this->settings->sizeRandom));
     }
@@ -110,6 +113,7 @@ void ParticleSystem::stepSimulation(float deltaTime, const glm::mat4 &emitterTra
         {
             // move with emitter
             particle.position = glm::vec3(emitterTransform * glm::vec4(particle.spawnPosition, 1.0f));
+            particle.velocity = glm::mat3(emitterTransform) * particle.spawnVelocity;
         }
     }
 }
