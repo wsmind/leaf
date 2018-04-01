3fb6e700fa84db7edcaceebcb94e469fef1ffcab
Remi Papillie <remi.papillie@gmail.com>
2018-04-01 15:29:33 +0200

    chromatic abberation

--- src/engine/render/shaders/postprocess.ps.hlsl
+++ src/engine/render/shaders/postprocess.ps.hlsl
@@ -9,11 +9,31 @@ struct POSTPROCESS_PS_OUTPUT
 	float4 color: SV_TARGET;
 };
 
+float2 offsetUV(float2 uv, float factor)
+{
+    uv = uv * 2.0 - 1.0;
+
+    float l = length(uv);
+    uv *= pow(l, factor);
+
+    uv = uv * 0.5 + 0.5;
+    return uv;
+}
+
 POSTPROCESS_PS_OUTPUT main(POSTPROCESS_PS_INPUT input)
 {
     POSTPROCESS_PS_OUTPUT output;
 
-    float3 radiance = radianceTexture.Sample(radianceSampler, input.uv).rgb;
+    // per-channel offset for chromatic abberation
+    float2 uvR = offsetUV(input.uv, 0.034);
+    float2 uvG = offsetUV(input.uv, 0.024);
+    float2 uvB = offsetUV(input.uv, 0.041);
+
+    float3 radiance = float3(
+        radianceTexture.Sample(radianceSampler, uvR).r,
+        radianceTexture.Sample(radianceSampler, uvG).g,
+        radianceTexture.Sample(radianceSampler, uvB).b
+    );
 
     // vignette
     //radiance *= pow(1.0 - length(input.uv - float2(0.5, 0.5)), 2.0);
